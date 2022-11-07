609186b69fec103c52470df073b12398a88088a0
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-11-07 09:21:28 -0800

    Added methods for the button and combobox

---
+++ ChuckNorrisJokes/ChuckNorris.cs
@@ -0,0 +1,20 @@
+namespace ChuckNorrisJokes
+{
+    public partial class ChuckNorris : Form
+    {
+        public ChuckNorris()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFetchJoke_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboCatergories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
